f479517b7fa4dda93dee215f2df0418055f294e0
Muhammad Aasim <aasim667711@gmail.com>
2024-06-30 23:01:34 +0500

    complete project

---
+++ script.js
@@ -0,0 +1,24 @@
+let input = document.getElementById('input')
+let buttons = document.querySelectorAll('button');
+
+let string = ""
+
+let arr = Array.from(buttons)
+arr.forEach(button => {
+    button.addEventListener('click', (e) => {
+
+        if (e.target.innerHTML == '=') {
+            string = eval(string);
+            input.value = string;
+        }
+        else if (e.target.innerHTML == 'AC') {
+            string = "";
+            input.valur = string;
+        }
+        else {
+            input += e.target.innerHTML;
+            input.value = string;
+        }
+
+    })
+})
